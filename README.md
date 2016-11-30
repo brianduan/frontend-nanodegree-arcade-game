f92d9c93314b2b9952eb45a7d13a845bda70754e
Brian <dcyyyyy@gmail.com>
2016-12-01 02:00:52 +1100

    init

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+1.Summary
+Frogger Arcade is a variation of the classic Frogger arcade game.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+2.Goals/Winning
+The objective of this game is to get the character across from the grassland to the water without getting hit by the enemy bugs as many times as possible. The user's score increases by one point for each successful attempt. If a bug hits the player, the score will reset to 0.
+
+3.Movement and Controls
+Users move the character by using the main up/right/left/down arrow keys on their keyboard.
--- js/app.js
+++ js/app.js
@@ -1,19 +1,55 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+//Create variables
+var tileWidth = 101;
+var tileHeight = 83;
+var canvasWidth = 505;
+var canvasHeight = 606;
+var picWdith = 101;
+var picHeight = 171;
+var collisionFlag = 0;
 
+//Create helper function for score tally
+var scorePlus = function (){
+    var score = Number(document.querySelector('.score-number').innerHTML);
+    score++;
+    document.querySelector('.score-number').innerHTML=score;
+}
+
+var scoreReset = function (){
+    document.querySelector('.score-number').innerHTML=0;
+}
+
+// Create Enemies class
+var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = -tileWidth;
+    //generate random y-coordinates and speed
+    this.y = (Math.floor(Math.random() * 3)+1)*tileHeight-10;
+    this.speed = (Math.floor(Math.random() * 3)+1)*tileWidth;
 };
 
-// Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += dt * this.speed;
+    var xDiff = Math.abs(this.x - player.x);
+    var yDiff = Math.abs(this.y - player.y);
+    if ((xDiff< tileWidth*0.7) && (yDiff < tileHeight*0.7)){
+        //collision occurs
+        collisionFlag = 1;
+        this.x = -tileWidth;
+        this.y = (Math.floor(Math.random() * 3)+1)*tileHeight-10;
+    }
+
+    //run off the canvas 
+    if (this.x > canvasWidth){
+        this.x = -tileWidth;
+        this.y = (Math.floor(Math.random() * 3)+1)*tileHeight-10;
+    }
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,15 +57,65 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+// Create player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.sprite = 'images/char-boy.png';
+    this.xInital = tileWidth*2;
+    this.yInitial = 606-110-101+15;
+    this.x = this.xInital;
+    this.y = this.yInitial;
+    this.speedX = tileWidth;
+    this.speedY = tileHeight;
+};
+
+
+Player.prototype.update = function(dt) {
+    if (collisionFlag == 1){
+        //collision occurs
+        this.x = this.xInital;
+        this.y = this.yInitial;
+        scoreReset();
+    }
+    collisionFlag = 0;
+
+    //reach the water
+    if (this.y < 0){
+        this.x = this.xInital;
+        this.y = this.yInitial;
+        scorePlus();
+    }
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(direction) {
+    if ((direction == 'left')&&(this.x>0)){
+        this.x -= this.speedX;
+    }
+    if ((direction == 'right')&&(this.x<404)){
+        this.x += this.speedX;
+    }
+    if ((direction == 'up')&&(this.y>0)){
+        this.y -= this.speedY;
+    }
+    if ((direction == 'down')&&(this.y<350)){
+        this.y += this.speedY;
+    }
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy(),new Enemy(),new Enemy(),new Enemy(),new Enemy()];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
